6a6a486485231ccc6b3da6dc939bb3974a1ecf4f
Daniyel Zhumankulov <daniyel.zhumankulov@nu.edu.kz>
2022-11-22 10:05:12 +0600

    feat: add a profile link

--- public/src/components/Header/component.js
+++ public/src/components/Header/component.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Link from "next/link";
 import styles from "./styles.module.css";
 import iconUser from "~assets/icon-user.svg";
 import Image from "next/image";
@@ -8,13 +9,18 @@ export const Header = ({ headerHeight }) => {
     <header ref={headerHeight}>
       <div className={styles.header}>
         <div className={styles.logo}>Apprentice</div>
-        <div>
-          {authorized ? (
-            <img src="/assets/kadirbek-sharau.jpg" className={styles.avatar} />
-          ) : (
-            <Image src={iconUser} />
-          )}
-        </div>
+        <Link href="/profile">
+          <div>
+            {authorized ? (
+              <img
+                src="/assets/kadirbek-sharau.jpg"
+                className={styles.avatar}
+              />
+            ) : (
+              <Image src={iconUser} />
+            )}
+          </div>
+        </Link>
       </div>
       <hr></hr>
     </header>
